a6d33078dc5f20a3f1c721d837c5fcd2d0e99eb1
KostyaPetukhov <91685050+KostyaPetukhov@users.noreply.github.com>
2021-10-07 13:24:22 +0300

    Сделано добавление и удаление строк и столбцов

---
+++ script.js
@@ -0,0 +1,46 @@
+﻿
+let table = document.getElementById('cube');
+
+function createCell(row) {
+  row.insertCell(-1);
+}
+
+function delCell(row) {
+  row.deleteCell(-1);
+}
+
+function addRow() {
+  let newRow = table.insertRow(-1);
+  let colsLength = table.rows[0].cells.length;
+  for (let i = 0; i < colsLength; i++) {
+    createCell(newRow);
+  }
+}
+
+function addCol() {
+  let rows = table.rows;
+  for (let i = 0, l = rows.length; i < l; i++) {
+    createCell(rows[i]);
+  }
+}
+
+function delRow() {
+    table.deleteRow(-1);
+}
+
+function delCol() {
+  let rows = table.rows;
+  for (let i = 0, l = rows.length; i < l; i++) {
+    delCell(rows[i]);
+  }
+}
+
+function showDeleteButton() {
+  document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
+  document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
+}
+
+function hideDeleteButton() {
+  document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
+  document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden";
+}
