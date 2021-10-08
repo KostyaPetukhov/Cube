789cf92770cac66eabde029b37fe71d70a7b513e
KostyaPetukhov <91685050+KostyaPetukhov@users.noreply.github.com>
2021-10-08 11:42:17 +0300

    Обновлена анимация удаления

--- script.js
+++ script.js
@@ -36,11 +36,26 @@ function delCol() {
 }
 
 function showDeleteButton() {
-  document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
+  let rows = table.rows.length;
+    if (rows <= 1) {
+    document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
+    document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden";
+    } else {
+      document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
+      document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
+    }
+  let cols = table.rows[0].cells.length;
+  if (cols <= 1) {
+  document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
   document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
+  } else {
+    document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
+    document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
+  }
 }
 
 function hideDeleteButton() {
   document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
   document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden";
 }
+
