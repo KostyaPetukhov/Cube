8d56ccfa645197a00b49a09a3da63d8217595196
KostyaPetukhov <91685050+KostyaPetukhov@users.noreply.github.com>
2021-10-19 20:41:15 +0300

    Финальная версия

--- script.js
+++ script.js
@@ -1,60 +1,106 @@
-﻿
-let table = document.getElementById('cube');
+﻿"use strict";
 
-function createCell(row) {
-  row.insertCell(-1);
-}
+const table = document.getElementById('cube');
+const deleteColsButton = document.getElementById('deleteColsButton');
+const deleteRowsButton = document.getElementById('deleteRowsButton');
+let matrix = {rows: 4, columns: 4};
+let currentIndex = {row: 0, column: 0};
+let rowIndex = 0;
+let cellIndex = 0;
 
-function delCell(row) {
-  row.deleteCell(-1);
-}
-
-function addRow() {
-  let newRow = table.insertRow(-1);
-  let colsLength = table.rows[0].cells.length;
-  for (let i = 0; i < colsLength; i++) {
-    createCell(newRow);
+const drawTable = () => {
+  for (let i = 0; i < matrix.rows; i++){
+    const tr = document.createElement('tr');
+      for (let j = 0; j < matrix.columns; j++){
+        const td = document.createElement('td');
+        tr.appendChild(td);
+      }
+    table.appendChild(tr);
   }
-}
+};
 
-function addCol() {
-  let rows = table.rows;
-  for (let i = 0, l = rows.length; i < l; i++) {
-    createCell(rows[i]);
-  }
-}
+drawTable();
 
-function delRow() {
-    table.deleteRow(-1);
-}
-
-function delCol() {
-  let rows = table.rows;
-  for (let i = 0, l = rows.length; i < l; i++) {
-    delCell(rows[i]);
-  }
-}
-
-function showDeleteButton() {
-  let rows = table.rows.length;
-  let cols = table.rows[0].cells.length;
-     if (rows <= 1 && cols <= 1) {
-    document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
-    document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden"; 
-    } else if (cols <= 1) {
-      document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
-      document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
-    } else if (rows <= 1) {
-      document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
-      document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden";
-    } else {
-      document.getElementsByClassName('deleteButton')[0].style= "visibility: visibility";
-      document.getElementsByClassName('deleteButton')[1].style= "visibility: visibility";
+table.onmouseover = function getCurrentIndex(e) {
+  let rowIndex = e.target.parentElement.rowIndex;
+  let cellIndex = e.target.cellIndex;
+  
+    if (rowIndex != undefined) {
+      currentIndex.row = rowIndex;
+      currentIndex.column = cellIndex;
+      deleteColsButton.style.left = `${cellIndex*37}px`;
+      deleteRowsButton.style.top = `${rowIndex*37}px`;
     }
-}
+};
 
-function hideDeleteButton() {
-  document.getElementsByClassName('deleteButton')[0].style= "visibility: hidden";
-  document.getElementsByClassName('deleteButton')[1].style= "visibility: hidden";
-}
+const addRow = () => {
+  const tr = document.createElement('tr');
+
+    for (let i = 0; i < matrix.columns; i++){
+      const td = document.createElement('td');
+      tr.appendChild(td);
+    }
+  table.appendChild(tr);
+  matrix.rows++;
+  console.log(matrix);
+};
+
+const addColumn = () => {
+  const rows = table.querySelectorAll('tr');
+
+  for (let i = 0; i < matrix.rows; i++) {
+    const td = document.createElement('td');
+    rows[i].appendChild(td);
+  }
+  matrix.columns++;
+  console.log(matrix);
+};
+
+const deleteRow = () => {
+  let currentRow = currentIndex.row;
+ 
+    if(matrix.rows > 1)
+    {
+      table.removeChild(document.querySelectorAll('tr')[currentRow]);
+      matrix.rows--;
+      console.log(matrix);
+    }
+    if (matrix.rows == currentIndex.row) {
+      deleteRowsButton.style.top = `${(currentIndex.row-1)*37}px`;
+      hideDeleteButton();
+    }
+};
+
+const deleteColumn = () => {
+  let currentColumn = currentIndex.column;
+  const rows = table.querySelectorAll('tr');
+  
+    if (matrix.columns > 1) {
+      for (let i = 0; i < rows.length; i++) {
+        rows[i].removeChild(rows[i].getElementsByTagName('td')[currentColumn]);
+      }
+      matrix.columns--;
+      console.log(matrix);
+    }
+    if (matrix.columns == currentIndex.column) {
+      deleteColsButton.style.left = `${(currentIndex.column-1)*37}px`;
+      hideDeleteButton();
+    }
+    
+};
+
+const showDeleteButton = () => {
+  if(matrix.columns > 1) {
+  deleteColsButton.style.visibility = "visible";
+  }
+  if (matrix.rows > 1 ) {
+  deleteRowsButton.style.visibility = "visible";
+  }
+};
+
+const hideDeleteButton = () => {
+  deleteColsButton.style.visibility = "hidden";
+  deleteRowsButton.style.visibility = "hidden";
+};
+
 
